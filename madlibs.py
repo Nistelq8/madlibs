44c4a53a4ec9c48e9fed85f53a37beaa83478753
Nistelq8 <aziz.almarzouq@gmail.com>
2022-07-03 19:51:44 +0300

    solved the task

--- madlibs.py
+++ madlibs.py
@@ -1,5 +1,22 @@
+from argparse import Action
+from time import time
+from tokenize import String
+from unicodedata import name
+
+
 def main():
 	# write your code here
+	
+	time = input("Please enter the time")
+	item = input("Please enter the item")
+	name = input("Please enter a name")
+	scream = input("Please enter a sentence")
+	action = input("please edter a verb")
+	story = f"""It was {time} o'clock when I heard a knock at the door.
+	I opened the door and there was a box full of {item} with a note saying From Mr. {name}.
+	Just as I closed the door I heard a scream {scream}
+	I froze in place and all I could do was {action}"""
+	print(story)
 
 
 
